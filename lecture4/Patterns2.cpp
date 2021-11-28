8c665ac9397c116f0300b88f361b7c0dae25d457
vaibhav223007 <vaibhavvishal223007@gmail.com>
2021-11-28 07:20:07 +0530

    pattern2

--- lecture4/Patterns2.cpp
+++ lecture4/Patterns2.cpp
@@ -193,7 +193,8 @@ int main() {
 
     //question10
 
-        int pattern;
+    /*
+    int pattern;
 
     cout << "Enter the number: ";
     cin >> pattern;
@@ -211,6 +212,213 @@ int main() {
         }
         cout <<  endl;
         row = row +1;
+    } */
+
+    //question11
+
+    /*
+    int pattern;
+
+    cout << "Enter the number" ;
+    
+    cin >> pattern;
+
+    int row = 1;
+    char count = 'A';
+
+    while (row <= pattern) {
+        int column = 1;
+        while ( column <= pattern){
+            cout << count;
+            column = column + 1;
+            count = count + 1;
+        }
+        cout << endl;
+        row = row + 1;
+    } */
+
+    //question12
+
+    /*
+    int pattern;
+
+    cout << "Enter a number";
+    cin >> pattern;
+
+    int row = 1;
+
+    while (row <= pattern ){
+        int column = 1;
+        while ( column <= pattern ){
+            char ch = row + column + 'A' - 2;
+            cout << ch;
+            column = column + 1;
+        }
+        cout << endl;
+        row = row + 1;
+    } */
+
+    //question13
+
+    /*
+    int pattern;
+
+    cout << "Enter a number";
+    cin >> pattern;
+
+    int row = 0;
+
+    while ( row < pattern ){
+        int column = 0;
+        while (column <= row){
+            char ch = 'A' + row;
+            cout << ch;
+            column = column + 1;
+        }
+        cout << endl;
+        row = row + 1;
+    } */
+
+    //question14
+
+    /*
+    int pattern;
+
+    cout << "Enter a number";
+    cin >> pattern;
+
+    int row = 1;
+
+    while ( row <= pattern ){
+        int column = 1;
+        while (column <= row){
+            char ch = 'A' + row + column -2;
+            cout << ch;
+            column = column + 1;
+        }
+        cout << endl;
+        row = row + 1;
+    } 
+    */
+
+    //question 15
+
+    /*
+    int pattern;
+
+    cout << "Enter a number";
+    cin >> pattern;
+
+    int row = 1;
+    int count = pattern;
+
+    while ( row <= pattern ){
+        int column = 1;
+        while (column <= row){
+            cout << count + column - 1;
+            column = column + 1;
+        }
+        
+        cout << endl;
+        row = row + 1;
+        count = count - 1;
+        
+    } */
+
+    //question 16
+
+    /*
+    int pattern;
+
+    cout << "Enter a number";
+    cin >> pattern;
+
+    int row = 1;
+    int count = pattern;
+
+    while ( row <= pattern ){
+        int column = 1;
+        while (column <= row){
+            char ch = 'A' + count + column -2;
+            cout << ch;
+            column = column + 1;
+        }
+        
+        cout << endl;
+        row = row + 1;
+        count = count - 1;
+        
+    } */
+
+    //question17
+
+    /*
+    int number;
+
+    cout << "Enter a number";
+    cin >> number;
+
+    int row = 1;
+
+    while (row <= number ){
+        int space = number - row;
+        while (space != 0){
+            cout<<" ";
+            space = space - 1;
+        }
+        int star = 1;
+        while (star <= row){
+            cout << '*';
+            star = star+1;
+        }
+
+        cout << endl;
+        row = row + 1;
     }
+    */
+
+    //question18
+
+    /*
+    int number;
+
+    cout << "ENter a number";
+    cin >> number;
+
+    int row = 1;
+    int count = number;
+
+    while (row <= number){
+        int column = 1;
+        while (column <= count){
+            cout << count - column + 1;
+            column = column +1;
+        }
+        cout << endl;
+        row = row + 1;
+        count = count -1;
+    } */
+
+    //question18
+
+    int number;
+
+    cout << "ENter a number";
+    cin >> number;
+
+    int row = 1;
+    int count = number;
+
+    while (row <= number){
+        int column = 1;
+        while (column <= count){
+            cout << '*';
+            column = column +1;
+        }
+        cout << endl;
+        row = row + 1;
+        count = count -1;
+    }
+
 
 }
